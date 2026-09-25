2ca800004aa33157dacf44f4abb0c075013608ca
sonu <safescan21@gmail.com>
2026-09-25 14:46:31 +0530

    Fix CartPage import path in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Searchpage from './components/pages/Searchpage';
 import ShopePage from './components/pages/ShopePage';
 import Contact from './components/pages/Contact';
 import Login from '../src/components/pages/Login';
-import CartPage from './components/cart/CheckoutPage';
+import CartPage from './components/cart/CartPage';
 import CheckoutPage from './components/cart/CheckoutPage';
 import SingleProduct from './components/shop/ProductDetails/SingleProduct';
 import ForgotPassword from './components/password/ForgotPassword';
